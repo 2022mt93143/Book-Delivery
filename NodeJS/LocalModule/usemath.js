c79d31f33b11b07f501e55cdb9f86acec5dc97d1
Nithin <51042622+nithin0@users.noreply.github.com>
2023-11-12 20:39:17 +0530

    Delete NodeJS/LocalModule directory

--- NodeJS/LocalModule/usemath.js
+++
@@ -1,9 +0,0 @@
-let mathmodule = require('./math')
-let regexmodule = require('./regularexpression')
-// console.log(mathmodule.area(2));
-// console.log(mathmodule.cir(3));
-
-
- console.log(regexmodule.result("12344"));
- console.log(regexmodule.emailres("aerw.eerte@ewerw.com"));
-
