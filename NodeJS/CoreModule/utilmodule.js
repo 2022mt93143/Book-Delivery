894dc494a767c6bc9dd92da059e5defe251710a9
Nithin <51042622+nithin0@users.noreply.github.com>
2023-11-12 20:38:58 +0530

    Delete NodeJS/CoreModule directory

--- NodeJS/CoreModule/utilmodule.js
+++
@@ -1,8 +0,0 @@
-//import urll = require('../CoreModule/URLmodule');
-
-var test = require('util');
-loc = 'Bengaluru'
-newloc = 'karnataka'
-let output = test.format("My current location is %s and my new location will be %s",loc,newloc)
-console.log(output);
-    
